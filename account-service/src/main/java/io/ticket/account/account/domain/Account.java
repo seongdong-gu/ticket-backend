d93cf5a0ae9ecaec80387b46a4a3d63ee142c0a6
Byeongju, Shin <404err@naver.com>
2024-02-20 12:45:43 +0900

    add: 계정 데이터 추가 구현

---
+++ account-service/src/main/java/io/ticket/account/account/domain/Account.java
@@ -0,0 +1,35 @@
+package io.ticket.account.account.domain;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class Account {
+
+  private Long id;
+
+  private String usernmae;
+
+  private String password;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+
+  private LocalDateTime deletedAt;
+
+  Account(
+      Long id,
+      String usernmae,
+      String password,
+      LocalDateTime createdAt,
+      LocalDateTime updatedAt,
+      LocalDateTime deletedAt) {
+    this.id = id;
+    this.usernmae = usernmae;
+    this.password = password;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+  }
+}

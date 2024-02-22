6651bc2208f15fd99294015a01088e19c23405a0
Byeongju, Shin <404err@naver.com>
2024-02-22 20:02:11 +0900

    style: 들여쓰기 수정

--- common/src/main/java/io/ticket/common/schema/HttpApiResponse.java
+++ common/src/main/java/io/ticket/common/schema/HttpApiResponse.java
@@ -1,12 +1,8 @@
 package io.ticket.common.schema;
 
-public record HttpApiResponse <T>(
-    int status,
-    String message,
-    T data
-) {
+public record HttpApiResponse<T>(int status, String message, T data) {
 
-    public static <T> HttpApiResponse<T> ok(final T token) {
-        return new HttpApiResponse<>(200, "OK", token);
-    }
+  public static <T> HttpApiResponse<T> ok(final T token) {
+    return new HttpApiResponse<>(200, "OK", token);
+  }
 }

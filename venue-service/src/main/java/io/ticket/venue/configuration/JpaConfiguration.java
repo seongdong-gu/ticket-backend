24e02706979efa1bfd9051889c54771dd95f6ac9
Byeongju, Shin <404err@naver.com>
2024-02-26 22:00:08 +0900

    feat: JPA 설정 추가

---
+++ venue-service/src/main/java/io/ticket/venue/configuration/JpaConfiguration.java
@@ -0,0 +1,8 @@
+package io.ticket.venue.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {}
